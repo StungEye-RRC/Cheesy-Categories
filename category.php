ee923a2476b607bd665169a1f63624a3c72992fa
Kyle Geske <stungeye@gmail.com>
2015-11-23 10:06:15 -0600

    Displaying cheeses by category.

--- category.php
+++ category.php
@@ -1,7 +1,27 @@
 <?php
     require('db_connect.php');
-    $sql = "SELECT * FROM categories";
+    $id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+    if (!isset($id) || !is_numeric($id)) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $sql = "SELECT * FROM categories WHERE id = :id LIMIT 1";
     $statement = $db->prepare($sql);
+    $statement->bindValue('id', $id);
+    $statement->execute();
+
+    if ($statement->rowCount() != 1) {
+        header('Location: index.php');
+        exit;
+    }
+
+    $category_name = $statement->fetch()['name']; 
+    
+    $sql = "SELECT * FROM cheeses WHERE category_id = :id";
+    $statement = $db->prepare($sql);
+    $statement->bindValue('id', $id);
     $statement->execute();
 ?>
 <!DOCTYPE html>
@@ -10,7 +30,7 @@
     <title>Cheesy Categories</title>
 </head>
 <body>
-    <h1>Cheesy Categories</h1>
+    <h1><?= $category_name ?> Cheese</h1>
     <?php if ($statement->rowCount() > 0): ?>
         <ul>
             <?php while ($row = $statement->fetch()): ?>
