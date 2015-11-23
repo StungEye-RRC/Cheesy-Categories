17b6b5fd260fa231b57e144a3add60b52e4c362e
Kyle Geske <stungeye@gmail.com>
2015-11-23 09:38:39 -0600

    HTML5 templates in place.

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cheesy Categories</title>
+</head>
+<body>
+    <h1>Cheesy Categories</h1>
+
+</body>
+</html>
---
+++ new_cheese.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Cheese</title>
+</head>
+<body>
+    <h1>New Cheese</h1>
+
+</body>
+</html>
