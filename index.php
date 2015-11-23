61d6eb4902e0cbb45dbf497a08f8972a08ab43ce
Kyle Geske <stungeye@gmail.com>
2015-11-23 11:06:55 -0600

    Demo JOIN on cheeses and categories.

--- index.php
+++ index.php
@@ -3,6 +3,14 @@
     $sql = "SELECT * FROM categories";
     $statement = $db->prepare($sql);
     $statement->execute();
+    $categories = $statement->fetchAll();
+
+    $sql = "SELECT categories.name AS category, cheeses.name AS cheese FROM cheeses " .
+           "INNER JOIN categories ON categories.id = cheeses.category_id ORDER BY cheese";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $cheeses_with_categories = $statement->fetchAll();
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -11,19 +19,21 @@
 </head>
 <body>
     <h1>Cheesy Categories</h1>
-    <?php if ($statement->rowCount() > 0): ?>
-        <ul>
-            <?php while ($row = $statement->fetch()): ?>
-                <li>
-                    <a href="category.php?id=<?= $row['id'] ?>">
-                        <?= $row['name'] ?>
-                    </a>
-                </li>
-            <?php endwhile ?>
-        </ul>
-    <?php else: ?>
-        <p>There are no cheese categories in the database.</p>
-    <?php endif ?>
+    <ul>
+        <?php foreach($categories as $category): ?>
+            <li>
+                <a href="category.php?id=<?= $category['id'] ?>">
+                    <?= $category['name'] ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+    <h2>Cheeses with Categories</h2>
+    <ul>
+        <?php foreach($cheeses_with_categories as $cheese): ?>
+            <li><?= $cheese['cheese'] ?> (<?= $cheese['category'] ?>)</li>
+        <?php endforeach ?>
+    </ul>
     <h2>Add To Database</h2>
     <p>
         <a href="new_cheese.php">New Cheese</a> - <a href="new_category.php">New Category</a>
